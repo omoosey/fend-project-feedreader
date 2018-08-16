8913435903bccbab46d9b548fa162b3b9feaef6c
Mariano Salamanca Jr <msalamancajr@gmail.com>
2018-08-15 20:36:08 -0400

    Updated 'New Feed Selection' to compare text of two feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,12 +82,21 @@ $(function() {
     /* Suite to test that selecting a new feed works */
     describe('New Feed Selection', function(){
        /* Test to make sure the content on the page has changed */ 
+        let firstFeed = '';
+        let secondFeed = '';
+
          beforeEach(function(done){
-            loadFeed(1, done);
+            loadFeed(0, function(){
+                firstFeed = $('.entry').text();
+            });
+            loadFeed(1, function(){
+                secondFeed = $('.entry').text();
+                done();
+            });
          });
 
          it('should change the content', function(){
-            expect($('.header-title').text()).toBe('CSS Tricks');
+            expect(firstFeed === secondFeed).toBe(false);
          });
      });
 
